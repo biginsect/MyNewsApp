b46ce6918050cdd2c80ce80ae26eb9ed7de95d61
lipeng-ds3 <lipeng-ds3@gomeplus.com>
2017-11-01 12:07:51 +0800

    更新LoginPageActivity，获取edittext内容时使用textutil检查是否为空，将登录验证部门封装成一个方法

--- app/src/main/java/com/example/lipeng_ds3/myapp/activity/LoginPageActivity.java
+++ app/src/main/java/com/example/lipeng_ds3/myapp/activity/LoginPageActivity.java
@@ -6,6 +6,7 @@ import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+import android.text.TextUtils;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -46,7 +47,8 @@ public class LoginPageActivity extends AppCompatActivity implements View.OnClick
     }
 
     private void getUserFromDB(){//从数据库读取内容
-        user = UserDatabase.getInstance(this).getUser().get(0);
+        database = UserDatabase.getInstance(this);
+        user = database.getUser().get(0);
         account = user.getAccount();
         password = user.getPassword();
     }
@@ -55,21 +57,27 @@ public class LoginPageActivity extends AppCompatActivity implements View.OnClick
     public void onClick(View v) {
         switch (v.getId()){
             case R.id.login:
-                getAccount = accountEdit.getText().toString();
-                getPassword = passwordEdit.getText().toString();
-                if (getAccount == null){//检查EditText内容是否为空
-                    Toast.makeText(this, "account must not be null!",Toast.LENGTH_SHORT ).show();
-                }else if (account.equals(getAccount) && password.equals(getPassword)){
-                    Intent intent = new Intent(LoginPageActivity.this, MainActivity.class);
-                    Toast.makeText(this, "登录成功", Toast.LENGTH_SHORT).show();
-                    startActivity(intent);
-                    finish();
-                }else {
-                    Toast.makeText(this, "account or password is invalid!",Toast.LENGTH_SHORT).show();
-                }
+                login();
                 break;
             default:
                 break;
+        }
+    }
+
+    private void login(){
+        //获取EditText的账号和密码
+        getAccount = accountEdit.getText().toString();
+        getPassword = passwordEdit.getText().toString();
+        if ((TextUtils.isEmpty(getAccount))
+                && (TextUtils.isEmpty(getPassword))){//检查EditText内容是否为空
+            Toast.makeText(this, "account must not be null!",Toast.LENGTH_SHORT ).show();
+        }else if (account.equals(getAccount) && password.equals(getPassword)){
+            Intent intent = new Intent(LoginPageActivity.this, MainActivity.class);
+            Toast.makeText(this, "Login successfully!", Toast.LENGTH_SHORT).show();
+            startActivity(intent);
+            finish();
+        }else {
+            Toast.makeText(this, "account or password is invalid!",Toast.LENGTH_SHORT).show();
         }
     }
 }
